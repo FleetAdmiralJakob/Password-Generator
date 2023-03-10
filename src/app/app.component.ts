318a03a55e25d3971c6f8d505d59b51066b7756d
Jakob Rössner <jakob.roessner@outlook.de>
2023-03-10 18:07:39 +0100

    .
    .

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  password = '';
+
+  submit() {
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+';
+  }
+
+  getPassword() {
+    return this.password
+  }
+}
