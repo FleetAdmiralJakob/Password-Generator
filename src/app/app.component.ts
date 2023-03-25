0a72abeaccf4451e809994535b50f2f6b940d056
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-03-25 18:15:03 +0100

    - Added nicer toasts
    - Added "copy to clipboard"
    
    - Added password security levels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,14 @@ export class AppComponent {
   letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   numbers = '0123456789';
   symbols = '!@#$%^&*()_+';
-
   includeNumbers = false;
   includeSymbols = false;
-
   passwordLength = 0;
+  passwordSecurityStatus = '';
+  showLengthSecurityStatus = false;
 
-  constructor() {
+
+  constructor(private toastr: ToastrService) {
     this.password = '';
     this.letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     this.numbers = '0123456789';
@@ -25,6 +27,8 @@ export class AppComponent {
     this.includeNumbers = false;
     this.includeSymbols = false;
     this.passwordLength = 0;
+    this.passwordSecurityStatus = '';
+    this.showLengthSecurityStatus = false;
   }
 
   calculatePassword(characters: string) {
@@ -36,7 +40,7 @@ export class AppComponent {
   submit() {
     this.password = '';
     if (this.passwordLength < 6) {
-      alert("Password length must be at least 6 characters long, otherwise it's not secure!");
+      this.toastr.error('Password length must be at least 6 characters long, otherwise it\'s not secure!', 'Error!');
       return;
     } else if (this.includeNumbers && this.includeSymbols) {
       const characters = this.letters + this.numbers + this.symbols;
@@ -55,24 +59,44 @@ export class AppComponent {
       this.calculatePassword(characters);
       return;
     } else {
-      alert("Something went wrong, please try again!");
+      this.toastr.error('Something went wrong, please try again!', 'Error!');
     }
     }
 
   changePasswordProperties(symbolsCheck?: any, numbersCheck?: any, length?: any) {
     if (symbolsCheck !== undefined) {
       this.includeSymbols = symbolsCheck.target.checked;
-      console.log("Symbols: " + this.includeSymbols);
     }
     if (numbersCheck !== undefined) {
       this.includeNumbers = numbersCheck.target.checked;
-      console.log("Numbers: " + this.includeNumbers);
-    } if (length !== undefined) {
+    }
+    if (length !== undefined) {
       this.passwordLength = length.target.value;
-      console.log("Length: " + this.passwordLength);
+      this.showLengthSecurityStatus = true;
     }
   }
   getPassword() {
     return this.password;
+  }
+
+  changePasswordSecurityStatus(length: any) {
+    console.log(length.target.value);
+    if (length.target.value < 6) {
+      this.passwordSecurityStatus = 'Not Secure';
+    } else if (length.target.value >= 6 && length.target.value < 12) {
+      this.passwordSecurityStatus = 'Secure';
+    } else if (length.target.value >= 12) {
+      this.passwordSecurityStatus = 'Very Secure';
+    }
+  }
+
+  copyToClipboard() {
+    const el = document.createElement('textarea');
+    el.value = this.password;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    this.toastr.success('Password copied to clipboard!', 'Success!')
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
