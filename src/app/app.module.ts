2b9899d0ff954e8e94b569355c3503fa269283d4
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-04-06 09:09:09 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
+import {NgOptimizedImage} from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
+        NgOptimizedImage,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
